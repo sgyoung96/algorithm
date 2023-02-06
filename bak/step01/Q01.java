1e997df52d85074a2978f6394426a89f00ffb8a9
gayeong <gayeong@192.168.0.17>
2023-02-06 21:55:11 +0900

    백준 알고리즘 단계별 풀이 1 의 1~2번 문제

---
+++ bak/step01/Q01.java
@@ -0,0 +1,13 @@
+package bak.step01;
+/*
+문제 : Hello World
+설명 : Hello World! 를 출력하시오.
+작성 : 달별 선장
+제출 : 패키지명을 제외하고, 클래스명은 Main 으로 바꾸어서 제출
+출처 : 백준 알고리즘
+*/
+public class Q01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
