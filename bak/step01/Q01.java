c729d8b028907553c4c4d1e41f41bcc3a47604f1
gayeong <gayeong@192.168.0.17>
2023-02-06 22:03:37 +0900

    누락 주석 추가

--- bak/step01/Q01.java
+++ bak/step01/Q01.java
@@ -3,7 +3,7 @@ package bak.step01;
 문제 : Hello World
 설명 : Hello World! 를 출력하시오.
 작성 : 달별 선장
-제출 : 패키지명을 제외하고, 클래스명은 Main 으로 바꾸어서 제출
+제출 : 패키지명을 제외하고, 클래스명은 Main 으로 바꾸어서 제출, import 문도 삽입해야 한다.
 출처 : 백준 알고리즘
 */
 public class Q01 {
