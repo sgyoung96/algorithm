50eb1c5110417f7c12ca126903d8f5c40e6175dc
gayeong <gayeong@localhost>
2023-02-14 12:10:50 +0900

    [백준] 단계별 풀기 - 입출력과 사칙연산 4

---
+++ bak/step01/Q04.java
@@ -0,0 +1,24 @@
+package bak.step01;
+
+import java.util.Scanner;
+
+/*
+문제 : A×B
+설명 : 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+	  첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+	  첫째 줄에 A×B를 출력한다.
+작성 : 달별 선장
+제출 : 패키지명을 제외하고, 클래스명은 Main 으로 바꾸어서 제출, import 문도 삽입해야 한다.
+출처 : 백준 알고리즘
+*/
+public class Q04 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String a = scanner.next();
+		String b = scanner.next();
+		scanner.close();
+		System.out.print(Integer.parseInt(a) * Integer.parseInt(b));
+	}
+
+}
